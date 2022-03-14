c9d2bbbe4595b20b6c92bc5fe5af35d67cdcf9b0
Vladislav Ponomarev <vlad11ponomarev@gmail.com>
2022-03-14 12:05:56 +0200

    update hometask

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeColor = document.querySelector('.change-color');
+changeColor.addEventListener('click', newColor);
+
+function newColor() {
+  const spanColor = getRandomHexColor();
+  document.body.style.backgroundColor = spanColor;
+}
+
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
